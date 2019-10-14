0558ee7e4e7d04ee0520ba78850d02770735ba4b
snurlan <52777942+snurlan@users.noreply.github.com>
2019-10-14 22:14:13 +0600

    Update myfamily.sh

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-echo $(curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq -r " .[] | select( .id == $HERO_ID ) | .connections.relatives" | tr -d \"ls)
+echo $(curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq -r " .[] | select( .id == $HERO_ID ) | .connections.relatives" | tr -d \")
