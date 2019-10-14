09d204b2f251fb9d2f4a0252f62e9f273c8559eb
snurlan <cg.nurlan@yandex.ru>
2019-10-14 22:01:11 +0600

    cl-camp4 fix

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,3 @@
-curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq -r " .[] | select( .id == $HERO_ID ) | .connections.relatives" | sed 's/"//g'
+#! /bin/bash
+
+curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq -r " .[] | select( .id == $HERO_ID ) | .connections.relatives" | tr -d \"
