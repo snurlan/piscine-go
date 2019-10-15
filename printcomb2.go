86c5e256d3a9fb141390a3a87370c03f8fd48ac8
Nurlan <cg.nurlan@gmail.com>
2019-10-16 01:10:12 +0600

    printcomb2 solution

---
+++ printcomb2.go
@@ -0,0 +1,33 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+
+	flag := false
+
+	for i := '0'; i <= '9'; i += 1 {
+		for j := '0'; j <= '9'; j += 1 {
+			for k := '0'; k <= '9'; k += 1 {
+				for l := '0'; l <= '9'; l += 1 {
+					if i == k && j == l {
+						continue
+					}
+					if flag {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					} else {
+						flag = true
+					}
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(' ')
+					z01.PrintRune(k)
+					z01.PrintRune(l)
+				}
+			}
+		}
+	}
+
+	z01.PrintRune('\n')
+}
