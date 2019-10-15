d4275353cd62cdbe6b22a40d59b11fc61cdbd077
Nurlan <cg.nurlan@gmail.com>
2019-10-16 01:38:12 +0600

    printnbr formatted

--- printnbr.go
+++ printnbr.go
@@ -9,9 +9,9 @@ func PrintNbr(n int) {
 	} else if n == 0 {
 		z01.PrintRune('0')
 	} else {
-		if n / 10 != 0 {
+		if n/10 != 0 {
 			PrintNbr(n / 10)
 		}
-		z01.PrintRune('0' + rune(n % 10))
+		z01.PrintRune('0' + rune(n%10))
 	}
 }
