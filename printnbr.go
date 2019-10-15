62df2a0572273c390c930ded718079e51c8321af
Nurlan <cg.nurlan@gmail.com>
2019-10-16 01:36:44 +0600

    printnbr solution

---
+++ printnbr.go
@@ -0,0 +1,17 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbr(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+		PrintNbr(-1 * n)
+	} else if n == 0 {
+		z01.PrintRune('0')
+	} else {
+		if n / 10 != 0 {
+			PrintNbr(n / 10)
+		}
+		z01.PrintRune('0' + rune(n % 10))
+	}
+}
