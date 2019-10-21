565d002b3365d5e372ef0af8360cec54d550bbe9
snurlan <cg.nurlan@yandex.ru>
2019-10-21 15:12:59 +0600

    iterativefactorial solution

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,13 +3,13 @@ package piscine
 func IterativeFactorial(nb int) int {
 	if nb < 0 {
 		return 0
-	} else if nb == 0 {
-		return 1
-	} else {
-		res := IterativeFactorial(nb-1) * nb
+	}
+	res := 1
+	for i := 2; i <= nb; i++  {
+		res *= i
 		if res < 0 {
 			return 0
 		}
-		return res
 	}
+	return res
 }
