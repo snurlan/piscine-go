4bbde08b67cf8c1f490b51b3160c23c090c5888a
snurlan <cg.nurlan@yandex.ru>
2019-10-21 15:15:46 +0600

    recursivefactorial solution

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,12 +1,12 @@
 package piscine
 
-func IterativeFactorial(nb int) int {
+func RecursiveFactorial(nb int) int {
 	if nb < 0 {
 		return 0
 	} else if nb == 0 {
 		return 1
 	} else {
-		res := IterativeFactorial(nb-1) * nb
+		res := RecursiveFactorial(nb-1) * nb
 		if res < 0 {
 			return 0
 		}
