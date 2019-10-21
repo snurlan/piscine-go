29cb4d5d132fb69f90b02e1945777461f7044fad
Nurlan <cg.nurlan@gmail.com>
2019-10-21 21:07:15 +0600

    index

---
+++ index.go
@@ -0,0 +1,19 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	sRunes := []rune(s)
+	toFindRunes := []rune(toFind)
+	for i, _ := range sRunes {
+		matches := true
+		for j, r2 := range toFindRunes {
+			if i+j >= len(sRunes) || sRunes[i+j] != r2 {
+				matches = false
+				break
+			}
+		}
+		if matches {
+			return i
+		}
+	}
+	return -1
+}
