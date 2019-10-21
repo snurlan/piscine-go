cc147483410b8e78fa5bab572223baabdf291184
snurlan <cg.nurlan@yandex.ru>
2019-10-21 15:20:10 +0600

    recfac fix

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,15 +1,11 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb < 0 {
+	if nb < 0 || nb > 20 {
 		return 0
 	} else if nb == 0 {
 		return 1
 	} else {
-		res := RecursiveFactorial(nb-1) * nb
-		if res < 0 {
-			return 0
-		}
-		return res
+		return RecursiveFactorial(nb-1) * nb
 	}
 }
