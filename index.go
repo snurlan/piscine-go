f923db7f6c0c0b9b207142f96ac142ebbad61c72
Nurlan <cg.nurlan@gmail.com>
2019-10-22 12:56:53 +0600

    index fix

--- index.go
+++ index.go
@@ -2,11 +2,12 @@ package piscine
 
 func Index(s string, toFind string) int {
 	sRunes := []rune(s)
+	n := StrLen(s)
 	toFindRunes := []rune(toFind)
 	for i := range sRunes {
 		matches := true
 		for j, r2 := range toFindRunes {
-			if i+j >= len(sRunes) || sRunes[i+j] != r2 {
+			if i+j >= n || sRunes[i+j] != r2 {
 				matches = false
 				break
 			}
