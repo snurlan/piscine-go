df3fbdd3d5f328fc7a06574baa4c194df1e1b2e2
Nurlan <cg.nurlan@gmail.com>
2019-10-22 10:13:11 +0600

    printnbrinorder fix

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -5,16 +5,16 @@ import "github.com/01-edu/z01"
 func PrintNbrInOrder(n int) {
 	if n < 0 {
 		z01.PrintRune('-')
-		z01.PrintRune(rune(-(n % 10)) + '0')
 		if n/-10 != 0 {
 			PrintNbrInOrder(n / -10)
 		}
+		z01.PrintRune(rune(-(n % 10)) + '0')
 	} else if n == 0 {
 		z01.PrintRune('0')
 	} else {
-		z01.PrintRune(rune(n%10) + '0')
 		if n/10 != 0 {
 			PrintNbrInOrder(n / 10)
 		}
+		z01.PrintRune(rune(n%10) + '0')
 	}
 }
