433f2c3d4cba48d64f98c657f5f00351a365cbe4
Nurlan <cg.nurlan@gmail.com>
2019-10-23 11:24:31 +0600

    nbrconvertalpha

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args[1:]
+	isUpper := args[0] == "--upper"
+	for i, arg := range args {
+		if i == 0 && isUpper {
+			continue
+		}
+		num := 0
+		invalid := false
+		for _, r := range arg {
+			if r < '0' || r > '9' {
+				invalid = true
+				break
+			}
+			num = 10*num + int(r-'0')
+		}
+		if invalid || num == 0 || num > 26 {
+			z01.PrintRune(' ')
+			continue
+		}
+		var r rune
+		if r = 'a' + rune(num-1); isUpper {
+			r = 'A' + rune(num-1)
+		}
+		z01.PrintRune(r)
+	}
+}
