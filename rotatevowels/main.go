3c9f6ca9fb42127c8a845b328dfc94423c4710b0
Nurlan <cg.nurlan@gmail.com>
2019-10-23 13:50:58 +0600

    rotatevowels

---
+++ rotatevowels/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func isVowel(r rune) bool {
+	return r == 'a' || r == 'o' || r == 'y' || r == 'u' || r == 'e' || r == 'i' || r == 'A' || r == 'O' || r == 'Y' || r == 'U' || r == 'E' || r == 'I'
+}
+
+func main() {
+	args := os.Args[1:]
+	var vowels []rune
+	cnt := 0
+	for _, arg := range args {
+		for _, r := range arg {
+			if isVowel(r) {
+				vowels = append(vowels, r)
+				cnt++
+			}
+		}
+	}
+
+	cur := 0
+	isFirst := true
+	for _, arg := range args {
+		if isFirst {
+			isFirst = false
+		} else {
+			z01.PrintRune(' ')
+		}
+		for _, r := range arg {
+			if isVowel(r) {
+				z01.PrintRune(vowels[cnt-cur-1])
+				cur++
+			} else {
+				z01.PrintRune(r)
+			}
+		}
+	}
+}
